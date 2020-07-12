17f1a6aa77becad16feca92c9b5ba038e52a1a06
darkness8129 <yaroslaw460@gmail.com>
2020-07-12 13:51:49 +0300

    added index and initial setup of js file

---
+++ matrix.js
@@ -0,0 +1,22 @@
+const canvas = document.getElementById('canvas'),
+    context = canvas.getContext('2d');
+// resize the canvas to fill browser window dynamically
+window.addEventListener('resize', resizeCanvas, false);
+
+function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    /**
+     * Your drawings need to be inside this function otherwise they will be reset when 
+     * you resize the browser window and the canvas goes will be cleared.
+     */
+    drawMatrix();
+}
+resizeCanvas();
+
+function drawMatrix() {
+
+
+
+}
